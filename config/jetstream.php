ae53f7ece016d2f4b94f3dab75696571810d17fd
ibrahim Tuna <tuna.cort52@gmail.com>
2024-07-19 15:15:24 +0300

    jetstream kaldırıldı(yayınlanan V1)

--- config/jetstream.php
+++ config/jetstream.php
@@ -58,11 +58,15 @@ return [
     */
 
     'features' => [
-        // Features::termsAndPrivacyPolicy(),
-        // Features::profilePhotos(),
-        // Features::api(),
-        // Features::teams(['invitations' => true]),
+         Features::termsAndPrivacyPolicy(),
+         Features::profilePhotos(),
+         Features::api(),
+         Features::teams(['invitations' => true]),
         Features::accountDeletion(),
+        'socialite' => [
+            'google' => true,
+        ],
+
     ],
 
     /*
